5fe2a74b1c0e4f6f1ec5cd709f2362df0a776014
GitHub Action <action@github.com>
2021-01-09 19:49:17 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ekrmh/intellijplatformdemoplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ekrmh.intellijplatformdemoplugin.services
+
+import com.github.ekrmh.intellijplatformdemoplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
